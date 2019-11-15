d7a2ee956f876983a28f62fb3890b1278f228e80
Paulo Khayat <paulokhayat@gmail.com>
2019-11-15 02:02:51 +0200

    Create utility genPoint method
    the name is temporary. could be replaced with genPoint_M for meters, ghPoint, or simply point.

--- src/main/java/robot/Utilities.java
+++ src/main/java/robot/Utilities.java
@@ -1,5 +1,9 @@
 package robot;
 
+import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d;
+import org.ghrobotics.lib.mathematics.units.LengthKt;
+import org.ghrobotics.lib.mathematics.units.Rotation2dKt;
+
 public class Utilities {
     /**
      * convert meters to radians of the drivetrain wheels
@@ -9,5 +13,17 @@ public class Utilities {
      */
     public static double metersToRadians(double meters) {
         return meters * Constants.Drivetrain.WHEEL_DIAMATER * Math.PI / 2;
+    }
+
+    /**
+     * A cleaner way to create a Pose2d object, since we work in meters and degrees.
+     * the name is temporary
+     * @param x x value of the Pose2d object in meters
+     * @param y y value of the Pose2d object in meters
+     * @param angle rotation of the point in degrees
+     * @return GHRobotics Pose2d object.
+     */
+    public static Pose2d genPoint(double x, double y, double angle){
+        return new Pose2d(LengthKt.getMeter(x), LengthKt.getMeter(y), Rotation2dKt.getDegree(angle));
     }
 }
