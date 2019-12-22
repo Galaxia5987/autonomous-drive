9bc26129d2041c9a0980f19697c581776a4b8e27
lior <lior5858@gmail.com>
2019-12-22 17:23:35 +0200

    Fix old unnoticed merge conflict

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -10,27 +10,22 @@ package robot; /**
 
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class Robot extends TimedRobot {
-
+    public static OI m_oi;
+    Command m_autonomousCommand;
+    SendableChooser<Command> m_chooser = new SendableChooser<>();
     @Override
     public void robotInit() {
         m_oi = new OI();
     }
 
-    }
+
 
     @Override
     public void disabledInit() {
-    }
-
-    @Override
-    public void autonomousInit() {
-        m_autonomousCommand = m_chooser.getSelected();
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.start();
-        }
-
     }
 
     @Override
@@ -48,9 +43,13 @@ public class Robot extends TimedRobot {
 
     @Override
     public void autonomousInit() {
+        m_autonomousCommand = m_chooser.getSelected();
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.start();
+        }
 
     }
-
+    
     @Override
     public void autonomousPeriodic() {
 
